57a7efd6abd2143170adf1794f70809b2e033bef
petros <pit.trak@gmail.com>
2021-05-30 18:38:35 +0300

    Batch processing pros and cons

--- 03-Concurrency-Patterns-in-Go/06-Pipelines/01-Batch-processing/main.go
+++ 03-Concurrency-Patterns-in-Go/06-Pipelines/01-Batch-processing/main.go
@@ -17,6 +17,15 @@ import "fmt"
 // A stage consumes and returns the same type.
 // A stage must be reified by the language so that it may be passed around. Functions
 // in Go are reified and fit this purpose nicely.
+//
+// Notice how each stage is taking a slice of data and returning a slice of data? These
+// stages are performing what we call batch processing. This just means that they operate
+// on chunks of data all at once instead of one discrete value at a time.
+//
+// There are pros and cons to batch processing. For now, notice that for the original data
+// to remain unaltered, each stage has to make a new slice of equal lenght to store the results
+// of its caclulations. That means that the memory footprint of our program at any one time is
+// doubled the size of the slice we send into the start of our pipeline.
 
 func main() {
 
@@ -41,7 +50,7 @@ func main() {
 	integers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 	// Here, our add and multiply stages satisfy all the properties of a pipeline stage:
-	// They both consume a slice of int and return a slice of int, and because Go has
+	// They both consume a slice of int and return a slice of int (batch processing), and because Go has
 	// reified functions, we can pass add and multiply around.
 	for _, v := range add(multiply(integers, 2), 1) {
 		fmt.Println(v)
