59285205536ec103e970eddfa68e1076a74c5df1
petros <pit.trak@gmail.com>
2021-06-02 11:24:35 +0300

    The or-done channel

---
+++ 03-Concurrency-Patterns-in-Go/06-Pipelines/08-The-Or-Done-channel/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+// We don't know if the fact that our goroutine was canceled means the channel
+// we're reading from has been canceled. For this reason, we need to wrap our
+// read from the channel with a select statement that also selects from a done
+// channel.
+func main() {
+	orDone := func(done, c <-chan interface{}) <-chan interface{} {
+		valStream := make(chan interface{})
+		go func() {
+			defer close(valStream)
+			for {
+				select {
+				case <-done:
+					return
+				case v, ok := <-c:
+					if ok == false {
+						return
+					}
+					select {
+					case valStream <- v:
+					case <-done:
+					}
+				}
+			}
+		}()
+
+		return valStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	myChan := make(chan interface{})
+	defer close(myChan)
+
+	for val := range orDone(done, myChan) {
+		fmt.Println(val)
+	}
+}
